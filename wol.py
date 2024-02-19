c1c1d2e56ec39a429da4f6ae3589462d7a696044
Vsb23 <v.birardi3@studenti.uniba.it>
2024-02-19 20:17:26 +0100

    Aggiunta animazione di caricamento (da finire)

--- wol.py
+++ wol.py
@@ -21,9 +21,10 @@ if ping(IP) == 1:
             print("Il ping ha dato esito positivo! VUoi essere reindirizzato alla pagina di controllo?");
             risposta = input();
             if risposta == "Y":
-               ## while()
+               while(m = 5)
                 str = print("Reindirizzamento in corso");
-                str = print(str) + print(".");
+                str += str + print(".");
+                m++;
 
 
         sys.exit();   
@@ -34,7 +35,7 @@ elif ping(IP) == 0:
     print("Il server è già attivo! Desideri essere reindirizzato alla pagina? [Y/N]")
     risposta = input()
     if risposta == "Y":
-          subprocess.run(["explorer", "https://192.168.1.62:8006/#v1:0:=node%2Fvsbserver:4:2::::7::"])
+        subprocess.run(["explorer", "https://192.168.1.62:8006/#v1:0:=node%2Fvsbserver:4:2::::7::"])
     else: 
         print("Va bene! Wol si chiuderà tra pochi istanti")
         sys.exit()
