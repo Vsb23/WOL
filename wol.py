09fcbebaff25d125fc3423a40359d84be2284d11
Vsb23 <stefanobirardi02@gmail.com>
2024-02-22 18:40:16 +0100

    Aggiunto file WTW per scritta figa iniziale
     creato ciclo per ping negativo, quando acceso
    
     fixare bug evento iniziale

--- wol.py
+++ wol.py
@@ -3,8 +3,11 @@ import sys
 import time
 
 IP = "192.168.1.62"
-
-
+m = 5
+print("█░█░█▀▀░█░░░█▀▀░█▀█░█▄█░█▀▀░░░▀█▀░█▀█░░░█░█░█▀█░█")
+print("█▄█░█▀▀░█░░░█░░░█░█░█░█░█▀▀░░░░█░░█░█░░░█▄█░█░█░█")
+print("▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░▀░░▀▀▀░░░▀░▀░▀▀▀░▀▀▀")
+print("Eseguo primo tentativo di accensione del VSB-Server")
 
 def ping (IP):
     result = subprocess.run(["ping", IP], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -14,20 +17,24 @@ if ping(IP) == 0:
     print("Il server non è attivo! Vuoi attivarlo? [Y/N]")
     risposta = input()
     if risposta.upper() == "Y":
-        subprocess.run(["WakeMeOnLan.exe", "/wakeup", IP])
         print("Attendo prima di verificare il ping")
         time.sleep(30)
-        subprocess.run(["ping", IP])
+        print("Eseguo il ping")
+        for m in range(5):
+            subprocess.run(["WakeMeOnLan.exe", "/wakeup", IP])
+            subprocess.run(["ping","-n", "1", IP])
+            m += 1
         if ping(IP) == 1:
             print("Il ping ha dato esito positivo! Vuoi essere reindirizzato alla pagina di controllo?")
             risposta = input()
             if risposta == "Y":
-                m = 5
                 while m < 5:
                     print("Reindirizzamento in corso" + "." * m)
                     m += 1
-            
-
+            else:
+                print("Va bene! WOL si chiuderà tra pochi istanti")
+                time.sleep(1)
+                sys.exit()
         sys.exit()
     elif risposta.upper() == "N":
         print("WOl si chiuderà tra pochi istanti")
@@ -40,8 +47,6 @@ elif ping(IP) == 1:
     else: 
         print("Va bene! Wol si chiuderà tra pochi istanti")
         sys.exit()
-    
-    
 else:
         print("Risposta non valida. Si prega di rispondere con 'Y' o 'N'.")
 
