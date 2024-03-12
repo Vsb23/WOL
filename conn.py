d9856ebf1f09e3108e9ddd2e54729f371044acac
Vsb23 <stefanobirardi02@gmail.com>
2024-03-12 01:00:39 +0100

    Definito andamento progetto

--- conn.py
+++ conn.py
@@ -2,8 +2,7 @@ from events import *
 import os
 from colorama import Fore
 
-os.system('cls')
-
+sw2 = "VSB-Server"
 
 def welcome(): 
     with open("writings\\VS.txt", "r", encoding="utf8") as wl_write:
@@ -11,4 +10,20 @@ def welcome():
         print(Fore.LIGHTBLUE_EX + out)
         print(Fore.RESET)
 
+def connection(): 
+    print("Scegliere tra le seguenti opzioni: \n 1) Accendere una VM \n 2) Accendere un container \n 3) MOdifica manuale")
+    risposta = input()
+    match risposta:
+        case "1": 
+            pass 
+        case "2":
+            pass
+        case "3":
+            print("Prova di connessione al " + Fore.LIGHTBLUE_EX + sw2 + Fore.RESET)
+            os.system('putty\plink.exe -ssh 192.168.1.62 -l root -pw Tism0987 ')
 
+
+def start():
+    os.system('cls')
+    welcome()
+    connection()
