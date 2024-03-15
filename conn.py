199fbf8ee53b088cc7c75c8d9419448fa19e5a6c
Vsb23 <stefanobirardi02@gmail.com>
2024-03-15 21:15:28 +0100

    Sistemato codice per gestione vms e containers

--- conn.py
+++ conn.py
@@ -11,13 +11,30 @@ def welcome():
         print(Fore.RESET)
 
 def connection(): 
-    print("Scegliere tra le seguenti opzioni: \n 1) Accendere una VM \n 2) Accendere un container \n 3) MOdifica manuale")
+    print("Scegliere tra le seguenti opzioni: \n 1) Accendere una VM \n 2) Accendere un container \n 3) Modifica manuale")
     risposta = input()
     match risposta:
         case "1": 
-            pass 
+            os.system('putty\plink.exe -ssh root@192.168.1.62 -pw Tism0987 qm list \n')
+            print("Inserire l'ID della VM che si desidera accendere")
+
+            risposta = input()
+            vm_id = os.system('\n putty\plink -ssh root@192.168.1.62 -pw Tism0987 qm start ' + risposta)
+
+            print(" Si è scelto di accendere la VM" + str(vm_id) + "\n Accensione in corso")
+            os.system('putty\plink -ssh root@192.168.1.62 -pw Tism0987 qm start ' + risposta)
+
+
         case "2":
-            pass
+            os.system('putty\plink.exe -ssh root@192.168.1.62 -pw Tism0987 qt list \n')
+            print("Inserire l'ID del container che si desidera accendere")
+
+            risposta = input()
+            vm_id = os.system('\n putty\plink -ssh root@192.168.1.62 -pw Tism0987 qt start ' + risposta)
+
+            print(" Si è scelto di accendere il container" + str(vm_id) + "\n Accensione in corso")
+            os.system('putty\plink -ssh root@192.168.1.62 -pw Tism0987 qm start ' + risposta)
+
         case "3":
             print("Prova di connessione al " + Fore.LIGHTBLUE_EX + sw2 + Fore.RESET)
             os.system('putty\plink.exe -ssh 192.168.1.62 -l root -pw Tism0987 ')
